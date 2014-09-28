056915685e705e297b90840244932e160ccba69c
Reinaldo Verdugo <Reinaldoverdugo@MacBook-Pro-de-Reinaldo.local>
2014-09-28 18:02:47 -0430

    Initial Commit

---
+++ HolaMundoTests/HolaMundoTests.swift
@@ -0,0 +1,36 @@
+//
+//  HolaMundoTests.swift
+//  HolaMundoTests
+//
+//  Created by Reinaldo Verdugo on 28/09/14.
+//  Copyright (c) 2014 ___PruebaCorp___. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class HolaMundoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
